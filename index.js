742830562d08e9c5e338fff1db2fe38dee3c002e
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-03-01 23:03:06 +0900

    2022-03-01

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = 8000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 
-const config = require('./config/key')
+const config = require('./config/key');
 
 // application/x-wwww-formurlencoded
 app.use(express.urlencoded({extended: true})); // body-parser 가 express에 내장
 // application/json
 app.use(express.json());
+app.use(cookieParser());
 
 const {User} = require('./models/User');
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
 mongoose.connect(config.mongoURI
   // {
     // useNewUrlParser: true,
@@ -21,25 +25,85 @@ mongoose.connect(config.mongoURI
     //  useFindAndModify: false
   // }
 ).then(() => console.log('MongoDb Connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello world!??'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 // 회원 가입을 위한 route
-app.post('/register', (req, res) => {
+// User 스키마를 맵핑한 모델(클래스)로 인스턴스 만들고, 암호화 후 db에 저장
+app.post('/api/users/register', (req, res) => {
   // 회원 가입할 때  필요한 정보들을 client에서 가져오면 
   // 그것들을 데이터베이스에 넣어준다.
-
-  const user = new User(req.body)
+  const user = new User(req.body);
 
   user.save((err, userInfo) => {
-    if(err) return res.json({ success: false, err})
+    
+    if (err) return res.json({success: false, err});
+    // console.log('userInfo', userInfo);
     return res.status(200).json({
       success: true
+    });
+  });
+});
+
+app.post('/api/users/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다
+  // 일치하는 데이터가 있으면, findOne() 메서드의 두번째 인자인 콜백함수에 
+  // 두 번째 값으로 전달된다.
+  // 찾는 값이 없으면 첫번째 인자에 err로,,
+  // 찾는 값이 있으면 해당 user 인스턴스가 두번쨰 인자로 들어감
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user) return res.json({
+      loginSuccess: false,
+      message: "제공된 이메일에 해당하는 유저가 없습니다."
+    });
+    // console.log(user); 해당 id user의 전체 데이터 담겨 있음
+
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+      // 비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        // token을 저장한다. 어디에 ? 쿠키 , 로컬스토리지
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId: user._id})
+      })
     })
   })
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// role 1 어드민 role 2 특정 부서 어드민
+// role 0 -> 일반유저 role이 0이 아니면 관계자
+
+app.get('/api/users/auth', auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdimin: req.user.role === 0 ? false: true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id},
+    {token: ""}
+    , (err, user) => {
+      if (err) return res.json({success: false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
